e800d9f3e9a2ff737d91a3f224013c0f7c766889
lukimann <lukewanyama5@gmail.com>
2024-02-14 09:56:12 +0300

    Sending Contacts

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import Contacts from './Contacts'
 import { Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Routes>
-        {/* Define the route tags here */}
+        <Route path="/contacts" element={<Contacts/>}/>
       </Routes>
     </div>
   );
---
+++ src/Contacts.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contacts = () => (
+    <div className= 'contacts'>
+        <h4>Sales & Marketing: Faith Ogendi: +254-745-346370</h4>
+        <h4>IT & Cybersecurity: Dennis Rotich: +254-110-931140 </h4>
+        <h4>Legal & Compliance: Luka Wanyama: +254-729-918181 </h4>
+        <h4>Finance & Accounting: Paul Thiongo: +254-270-99073</h4>
+    </div>
+)
+export default Contacts;
