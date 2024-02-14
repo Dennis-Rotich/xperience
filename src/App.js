2df1e3ee24e3c045034a94ada2af2781e9b6340a
Dennis <denkipyator@gmail.com>
2024-02-14 13:57:17 +0300

    configure git with navbar branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React,{useEffect,useState} from 'react';
 import './App.css';
-import Home from './components/Home';
 import Contacts from './components/Contacts';
 import { Routes, Route } from 'react-router-dom';
 import About from './components/About'; // Assuming you want to include the About component
 import Navbar from './components/Navbar'; // Assuming you want to include the Navbar component
+import Home from './components/Home';
 
 function App() {
   const [products,setProducts]=useState([])
@@ -24,9 +24,9 @@ function App() {
       <Navbar/>
       <Routes>
         {/* Define the route tags here */}
-        <Route path='/' element={<Home products={products}/>}/>
         <Route path="/contacts" element={<Contacts/>}/>
         <Route path="/about" element={<About/>}/> 
+        <Route path='/' element={<Home products={products}/>}/>
       </Routes>
     </div>
   );
