9b436701aac3c21b094ad68b59530d6f94e7a935
Dennis <denkipyator@gmail.com>
2024-02-14 17:33:34 +0300

    Config Navbar with Dev

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useEffect,useState} from 'react';
+import React,{useEffect,useState} from 'react';
 import { Routes,Route } from 'react-router-dom';
 import Home from './components/Home.jsx';
 import './App.css'
