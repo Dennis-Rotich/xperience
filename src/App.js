ca0254f6cfda3202d54d53a28d8e156bfc81d65f
Dennis <denkipyator@gmail.com>
2024-02-14 09:48:20 +0300

    Added Navbar to components file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Routes,Route } from 'react-router-dom';
-import Navbar from './Navbar';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
