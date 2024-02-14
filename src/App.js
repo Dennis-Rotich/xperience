3644d6ef9acbc56a709e16d5971b56778ea2974f
paul <paul.thiongo@student.moringaschool.com>
2024-02-14 10:15:56 +0300

    About

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
-import { Routes,Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import About from './components/About'; // Importing About component
+
 
 function App() {
   return (
     <div>
       <Routes>
-        {/* Define the route tags here */}
+        <Route path="/about" element={<About />} /> {/* Route for About component */}
+        
       </Routes>
     </div>
   );
