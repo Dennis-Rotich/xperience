080b1399e86c1fa64d9555db386dd44dc3fa3632
Dennis <denkipyator@gmail.com>
2024-02-14 09:27:22 +0300

    NavBar component

---
+++ src/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function Navbar(){
+    return(
+        <div id="navbar">
+            <NavLink to='/' className='navlink'>Home</NavLink> 
+            <NavLink to='/contact' className='navlink'>Contact</NavLink>
+            <NavLink to='/about' className='navlink'>About</NavLink>
+            <NavLink to='/signup' className='navlink'>Sign up</NavLink>
+            <form id="searchBar">
+                <input type="text" placeholder="Search for product" name="search"/>
+                <input type="submit" name="submit"/>
+            </form>
+        </div>
+    )
+}
+
+export default Navbar
