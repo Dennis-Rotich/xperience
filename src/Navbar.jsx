8560390ee2c1afd8d48d2c7bc8123bf33409eb65
Dennis <denkipyator@gmail.com>
2024-02-14 09:37:06 +0300

    Changes to the paths in Navlink tag

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,9 +5,9 @@ function Navbar(){
     return(
         <div id="navbar">
             <NavLink to='/' className='navlink'>Home</NavLink> 
-            <NavLink to='/contact' className='navlink'>Contact</NavLink>
+            <NavLink to='/contacts' className='navlink'>Contact</NavLink>
             <NavLink to='/about' className='navlink'>About</NavLink>
-            <NavLink to='/signup' className='navlink'>Sign up</NavLink>
+            <NavLink to='/SignUpForm' className='navlink'>Sign up</NavLink>
             <form id="searchBar">
                 <input type="text" placeholder="Search for product" name="search"/>
                 <input type="submit" name="submit"/>
