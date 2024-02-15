cdb8a53d659ff236f97f3ff59beb1abc988e8ec8
Dennis <denkipyator@gmail.com>
2024-02-15 12:09:37 +0300

    Configure css and Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import {Link} from 'react-router-dom';
 
 function Home({products}){
+    function handleClick(product){
+        console.log(product);
+    }
+
     const displayProducts = products.map((product,index)=>{
         return(
-            <span className="productDisplay" key={index}>
+            <span className="productDisplay" key={index} onClick={()=>{handleClick(product)}}>
                 <img src={product.image} alt={product.title}/>
                 <p>Price: ${product.price}</p>
             </span>
@@ -15,7 +19,7 @@ function Home({products}){
         <div>
             <aside>
                 <Link className="productList">Products</Link>
-                <Link className="productList">Payments</Link>
+                <Link className="productList" to='/payments'>Payments</Link>
                 <Link className="productList">Cart</Link>
                 <Link className="productList">Social Media</Link>
             </aside>
