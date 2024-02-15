727dbcd92c9301521728f85a59d9af7898603282
lukimann <lukewanyama5@gmail.com>
2024-02-15 11:26:42 +0300

    coming right up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
 import Contacts from './components/Contacts'
+import Payments from './components/Payments'
 import { Routes,Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div>
+      
       <Routes>
+        <Route path="/payments" element={<Payments/>}/>
         <Route path="/contacts" element={<Contacts/>}/>
-      </Routes>
+      </Routes> 
     </div>
   );
 }
