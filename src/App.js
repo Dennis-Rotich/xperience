0805c23f0c4d6ed0d32efc72dea6f51b395a94d6
Dennis <denkipyator@gmail.com>
2024-02-15 09:32:06 +0300

    Navbar component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import './App.css';
 import Contacts from './components/Contacts';
 import { Routes, Route } from 'react-router-dom';
 import About from './components/About'; // Assuming you want to include the About component
 import Navbar from './components/Navbar'; // Assuming you want to include the Navbar component
 
+
 function App() {
+  const [products,setProducts]=useState([])
+
+  useEffect(()=>{
+    fetch('https://fakestoreapi.com/products')
+    .then(res => res.json())
+    .then((data)=>{
+      setProducts(data)
+    })
+    .catch(err => console.error(err))
+  },[])
+
   return (
     <div>
       <h1>Xperience</h1>
       <Navbar/>
       <Routes>
+        <Route path='/' element={<Home products={products}/>}/>
         <Route path="/contacts" element={<Contacts/>}/>
         <Route path="/about" element={<About/>}/> 
       </Routes>
